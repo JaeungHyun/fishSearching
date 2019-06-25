17b8f364973d8450d6071fa86d19fe0144fef18a
JaeUng Hyun <jaeung@me.com>
2019-06-25 21:59:22 +0900

    [close #2]  카메라, 앨범 선택 기능 구현

--- FishSearching/FishSearching/ViewController.swift
+++ FishSearching/FishSearching/ViewController.swift
@@ -8,13 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    // MARK: - 사진 선택관련
+    @IBOutlet var preview: UIImageView!
+    
+    @IBAction func selectPhoto(_ sender: Any) {
+        let alert = UIAlertController(title: nil, message: "사진을 가져올 곳을 선택해 주세요", preferredStyle: .actionSheet)
+        
+        // 카메라를 사용할 수 있으면
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            alert.addAction(UIAlertAction(title: "카메라", style: .default) { (_) in
+                self.imgPicker(.camera)
+            })
+        }
+        // 저장된 앨범을 사용할 수 있으면
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) {
+            alert.addAction(UIAlertAction(title: "저장된 앨범", style: .default) { (_) in
+                self.imgPicker(.savedPhotosAlbum)
+            })
+        }
+        // 포토 라이브러리를 사용할 수 있으면
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            alert.addAction(UIAlertAction(title: "포토 라이브러리", style: .default) { (_) in
+                self.imgPicker(.photoLibrary)
+            })
+        }
+        
+        // 취소 버튼 추가
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+        
+        // 액션 시트 창 실행
+        self.present(alert, animated: true)
+    
+    }
+    
+    func imgPicker(_ source: UIImagePickerController.SourceType) {
+        let picker = UIImagePickerController()
+        picker.sourceType = source
+        picker.delegate = self
+        picker.allowsEditing = true
+        self.present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        self.preview.image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+        
+        picker.dismiss(animated: false)
+    }
 }
 
